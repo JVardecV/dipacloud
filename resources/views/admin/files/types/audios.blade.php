5f871603cd3c071d82de64853bbb7292cbe8cb70
JVardecV <nacho12442@gmail.com>
2019-05-20 21:49:45 -0400

    Lógica de manipulación de archivos terminada

--- resources/views/admin/files/types/audios.blade.php
+++ resources/views/admin/files/types/audios.blade.php
@@ -10,11 +10,31 @@
 		<div class="col-sm-12 cold-md-4 pb-4">
 			 <audio src="{{ asset('storage') }}/{{ $folder }}/audio/{{ $audio->name }}.{{ $audio->extension }}" controls>
 			 </audio>
-			 <form action="{{ route('file.destroy',$audio->id) }}" method="POST">
-				@csrf
-				<input type="hidden" name="_method" value="PATCH">
-				<button class="btn btn-danger float-right" type="submit"><i class="fas fa-trash"></i> Eliminar</button>
-			</form>
+			<button class="btn btn-danger pull-right" type="submit" data-toggle="modal" data-target="#deleteModal"><i class="fas fa-trash"></i> Eliminar</button>
+			<!-- Modal -->
+			<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-backdrop="false">
+			  <div class="modal-dialog" role="document">
+			    <div class="modal-content">
+			      <div class="modal-header">
+			        <h5 class="modal-title" id="exampleModalLabel">Confirmar eliminación</h5>
+			        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			          <span aria-hidden="true">&times;</span>
+			        </button>
+			      </div>
+			      <div class="modal-body">
+			        ¿Desea eliminar este archivo?
+			      </div>
+			      <div class="modal-footer">
+			        <button type="button" class="btn btn-success" data-dismiss="modal"><i class="fas fa-times"></i> Cancelar</button>
+			        <form action="{{ route('file.destroy',$audio->id) }}" method="POST">
+						@csrf
+						<input type="hidden" name="_method" value="PATCH">
+						<button class="btn btn-danger float-right" type="submit"><i class="fas fa-trash"></i> Eliminar</button>
+					</form>
+			      </div>
+			    </div>
+			  </div>
+			</div>
 		</div>
 	@empty
 	<div class="container mb-3">
@@ -27,6 +47,5 @@
 	</div>
 </div>
 
-
-
 @endsection
+
--- resources/views/admin/files/types/documents.blade.php
+++ resources/views/admin/files/types/documents.blade.php
@@ -41,14 +41,31 @@
 			 				@endif
 			 				
 			 			<th scope="row">
-							<form action="{{ route('file.destroy',$document->id) }}" method="POST">
-								@csrf
-								<input type="hidden" name="_method" value="PATCH">
-								<button class="btn btn-danger float-right" type="submit"><i class="fas fa-trash"></i> Eliminar</button>
-							</form>
-
-
-							{{-- <a class="btn btn-danger float-right" href="#"><i class="fas fa-trash"></i> Eliminar</a> --}}
+							<button class="btn btn-danger pull-right" type="submit" data-toggle="modal" data-target="#deleteModal"><i class="fas fa-trash"></i> Eliminar</button>
+							<!-- Modal -->
+							<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-backdrop="false">
+							  <div class="modal-dialog" role="document">
+							    <div class="modal-content">
+							      <div class="modal-header">
+							        <h5 class="modal-title" id="exampleModalLabel">Confirmar eliminación</h5>
+							        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+							          <span aria-hidden="true">&times;</span>
+							        </button>
+							      </div>
+							      <div class="modal-body">
+							        ¿Desea eliminar este archivo?
+							      </div>
+							      <div class="modal-footer">
+							        <button type="button" class="btn btn-success" data-dismiss="modal"><i class="fas fa-times"></i> Cancelar</button>
+							        <form action="{{ route('file.destroy',$document->id) }}" method="POST">
+										@csrf
+										<input type="hidden" name="_method" value="PATCH">
+										<button class="btn btn-danger float-right" type="submit"><i class="fas fa-trash"></i> Eliminar</button>
+									</form>
+							      </div>
+							    </div>
+							  </div>
+							</div>
 			 			</th>
 			 		</tr>
 			 	</tbody>
@@ -69,3 +86,4 @@
 
 
 @endsection
+
